10cf645a8f5921e24b1b973f809c8e28018e7dad
Lyseslukker <49211303+Lyseslukker@users.noreply.github.com>
2022-11-29 12:56:36 +0100

    React-query installed & setup react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>hahahaha</h1>
-    </div>
-  );
-}
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import Product from './components/Product/Product'
 
-export default App;
+export default function App() {
+
+
+
+
+
+  
+  return (
+    <div className='app'>
+      <Routes>
+        {/* <Route path='/' element={<Home />} />
+        <Route path='/homepage' element={<Home />} /> */}
+        <Route path='/product' element={<Product />} />
+        {/* <Route path='/productdetails' element={<Home />} />
+        <Route path='/productcomparison' element={<Home />} />
+        <Route path='/cart' element={<Home />} />
+        <Route path='/payment' element={<Home />} />
+        <Route path='/invoice' element={<Home />} />
+        <Route path='/login' element={<Home />} />
+        <Route path='/create' element={<Home />} />
+        <Route path='/profile' element={<Home />} />
+        <Route path='/orders' element={<Home />} />
+        <Route path='/contact' element={<Home />} />
+        <Route path='/about' element={<Home />} />
+        <Route path='/moreinfo' element={<Home />} /> */}
+      </Routes>
+    </div>
+  )
+}
