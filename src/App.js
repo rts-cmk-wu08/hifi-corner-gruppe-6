eebb5067ea26f3292035f8a05287f4632756c7af
flaschDK <U2405945@rts-365.dk>
2022-11-29 13:20:05 +0100

    lidt oprydning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import AvesomeIcon from "./components/AwesomeIcon";
 import Footer from "./templates/Footer";
-
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
 
 function App() {
   return (
