aade2684db88011923fbbf2dc8e7ab4199d8c360
Manissha <manissha52@yahoo.dk>
2022-11-29 13:09:43 +0100

    made components for MoreInfo

---
+++ src/components/MoreInfo/MoreInfoExchanges.js
@@ -0,0 +1,10 @@
+const MoreInfoExchanges = () => {
+    return ( 
+        <>
+        <h3>EXCHANGES</h3>
+        <p>We can only replace items if they are defective or damaged. If you need to exchange the product for the same item due to a fault, please first send us an email at sales@hificorner.co.uk and once approved, send your item to: Hi-Fi Corner, 2 Joppa Road, Mackenzie House, Portobello, Edinburgh, EH15 2EU. Please also include the proof of purchase and a cover letter detailing the reason for exchange.</p>
+        </>
+     );
+}
+ 
+export default MoreInfoExchanges;
---
+++ src/components/MoreInfo/MoreInfoShipping.js
@@ -0,0 +1,10 @@
+const MoreInfoShipping = () => {
+    return ( 
+        <>
+        <h3>SHIPPING</h3>
+        <p>To return your product, you should mail your product to: 2 Joppa Road, Mackenzie House, Edinburgh, EH15 2EU. If you would like to return an item, please note that we cannot arrange a collection of the item without additional cost; you will be responsible for ensuring the product reaches us. Shipping costs are non-refundable. If you receive a refund, the cost of return shipping will be deducted from your refund. Depending on where you live, the time it may take for your exchanged product to reach you may vary. We will process the refund when the item arrives with us. If you are shipping an item over £100, we would advise using a trackable shipping service or purchasing shipping insurance. We cannot guarantee that we will receive your returned item.</p>
+        </>
+     );
+}
+ 
+export default MoreInfoShipping;
