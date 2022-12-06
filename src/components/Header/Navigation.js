9c2de82486d2e13e447e8058d16bc823a6f6b2d5
Manissha <manissha52@yahoo.dk>
2022-12-06 11:10:04 +0100

    added code

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,24 +1,51 @@
-import { BsFillCartFill } from "react-icons/bs";
+import { HiShoppingCart } from "react-icons/hi";
 import { BsFillPersonFill } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
+import "../Header/Navigation.css"
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const Navigation = () => {
+    const product = new useQuery(["products"], () =>{
+        return axios("http://localhost:4000/category")
+                .then( (res) =>{
+                    return res.data
+                })
+    })
+
+    if (product.isError) {
+        console.log("error")
+        
+    }
+    if (product.isLoading) {
+        console.log("loading")
+        
+    }
+
     return (
         <>
-        <nav>
-        <ul>
-            <img src="" alt="" />
+        <nav className="navbar">
+          <ul className="navi">
+            <img src="images/logo.png" alt="logo" />
             <li><a href="#">SHOP</a></li>
             <li><a href="#">ABOUT US</a></li>
             <li><a href="#">CONTACT US</a></li>
-        </ul>
-        <form>
+          </ul>
+          {/* <form className="searchbar">
             <input type="text" placeholder="Search product..."/>
-            <input type="submit" value="send" />
-        </form>
-        <ul>
-            <li><a href="#"><BsFillCartFill/></a></li>
+            <input className="" type="submit" value="send" />
+          </form> */}
+          <div className="container">
+            <form className="searchbar" action="">
+                <input type="text" placeholder="Search product..." name="q"/>
+                <button type="submit"><FaSearch/></button>
+            </form>
+            
+          <ul className="navicons">
             <li><a href="#"><BsFillPersonFill/></a></li>
-        </ul>
+            <li><a href="#"><HiShoppingCart/></a></li>
+          </ul>
+          </div>
         </nav>
         
         </>
---
+++ src/components/Header/ShoppingList.js
@@ -0,0 +1,19 @@
+const ShoppingList = () => {
+    return ( 
+        <>
+        <h3>Cart</h3>
+        <p>(Amount of items)</p>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+        <p>Sub total:</p>
+        <p>(Price)</p>
+        <button type="submit">Go to cart</button>
+        <button type="submit">Go to payment</button>
+        
+        </>
+     );
+}
+
+export default ShoppingList;
