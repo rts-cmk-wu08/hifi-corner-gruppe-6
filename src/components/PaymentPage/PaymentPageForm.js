bf2427df061b02eed51f8c517ba3e69b91319591
Ramadan Cilomba <rcilomba@yahoo.com>
2022-12-12 09:04:29 +0100

    adding values for each input field in the form

--- src/components/PaymentPage/PaymentPageForm.js
+++ src/components/PaymentPage/PaymentPageForm.js
@@ -1,69 +1,61 @@
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import React,{useState}from 'react';
 
-const PaymentPageForm = () => {
+function PaymentPageForm(){
+  const [values, setValues] = useState({
+    fullName: "",
+    city: "",
+    postalCode: "",
+    address: "",
+    email: "",
+    phoneNumber: ""
+  })
 
+  return(
 
-const [fullName, setFullName] = useState();
-const [city, setCity] = useState();
-const [postalCode, setPostalCode] = useState();
-const [address, setAddress] = useState();
-const [email, setEmail] = useState();
-const [phoneNumber, setPhoneNumber] = useState();
-
-    return ( 
-        <div className='PaymentPageForm'>
+     <div className='PaymentPageForm'>
         <h2 className='PaymentPageHeader'>Your info</h2>
 
 
 <div className='PaymentContainer'>
     <div className='PaymentContainerBox'>
+
         <form>
       <label>Fullname <span className='red'>*</span> <br/>
         <input
-          type="text" 
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-        />
+        value={values.fullName}
+          />
+         
+    
       </label><br/>
 
       <label>City <span className='red'>*</span><br/>
         <input className='cityLabel'
-          type="text" 
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
+        value={values.city}
         />
       </label>
 
         <label>Postal code <span className='red'>*</span> 
         <input className='postalCodeLabel'
-          type="number" 
-          value={postalCode}
-          onChange={(e) => setPostalCode(e.target.value)}
+        value={values.postalCode}
+        
         />
       </label> <br/>
 
       <label>Address <span className='red'>*</span> <br/>
         <input
-          type="text" 
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
+        value={values.address}
+        
         />
       </label> <br/>
 
       <label>E-mail <span className='red'>*</span> <br/>
         <input
-          type="text" 
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+        
         />
       </label> <br/>
 
       <label>Phone nr. <span className='red'>*</span> <br/>
         <input
-          type="number" 
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
         />
       </label> <br/>
 
@@ -77,15 +69,14 @@ const [phoneNumber, setPhoneNumber] = useState();
         </div>
         <p>Text</p>
         <p>Text</p>
-        <button>Checkout</button>
+        <button type='submit'>Checkout</button>
         
     </div>
     
 </div>
         
         </div>
-        
-     );
+  );
 }
- 
+
 export default PaymentPageForm;
